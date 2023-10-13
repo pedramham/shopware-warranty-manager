8fe06b7ddc35106363962da7d9f28747b5f91779
morteza <pedramham1@gmail.com>
2023-10-13 12:35:06 +0300

    first commit

---
+++ tests/TestBootstrap.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+use Shopware\Core\TestBootstrapper;
+
+$loader = (new TestBootstrapper())
+    ->addCallingPlugin()
+    ->addActivePlugins('WarrantyManager')
+    ->setForceInstallPlugins(true)
+    ->bootstrap()
+    ->getClassLoader();
+
+$loader->addPsr4('WarrantyManager\\Tests\\', __DIR__);
